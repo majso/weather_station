ffdacc6728dfc42b78f3547c26612d38fedf7166
majso <michal.grich@gmail.com>
2024-08-16 19:01:18 +0200

    updated sensors

--- main.c
+++ main.c
@@ -8,11 +8,13 @@
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+/*
 #define SPI_PORT spi0
 #define PIN_MISO 16
 #define PIN_CS   17
 #define PIN_SCK  18
-#define PIN_MOSI 19
+#define PIN_MOSI 19 
+*/
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
@@ -29,7 +31,9 @@ int main()
     printf("Hello, IoT world from RP2040!\n");
     
     // Initialize sensors and radio
+    printf("Sensors starting..\n");
     sensors_init();
+    printf("Radio starting..\n");
     radio_init();
 
     while (true) {
--- sensors.c
+++ sensors.c
@@ -10,6 +10,7 @@
 void sensors_init() {
     // Initialize I2C (assuming default I2C0 pins)
     i2c_init(i2c0, 100 * 1000); // 100kHz
+    printf("Starting i2c\n");
     gpio_set_function(4, GPIO_FUNC_I2C);
     gpio_set_function(5, GPIO_FUNC_I2C);
     gpio_pull_up(4);
@@ -19,14 +20,17 @@ void sensors_init() {
     INA219 ina219_battery;
     INA219 ina219_solar;
     ina219_init(&ina219_battery, i2c0, 0x41);
+    printf("Starting ina219_battery\n");
+
     ina219_init(&ina219_solar, i2c0, 0x40);
+    printf("Starting ina219_solar\n");
 
     // Initialize SHT30 sensor
     sht30_init(i2c0, 0x44); // Replace with your SHT30 address if different
+    printf("Starting sht30\n");
     sht30_soft_reset();
 }
-
-// Read current from INA219 sensor
+// Read current from INA219 sensor// Read current from INA219 sensor
 float read_current_from_ina219(INA219 *ina219, uint8_t address) {
     ina219_init(ina219, i2c0, address);
     return ina219_read_current(ina219);
@@ -47,33 +51,25 @@ float read_voltage_from_ina219(INA219 *ina219, uint8_t address) {
 // Read temperature from SHT30 sensor
 float read_temperature_from_sht30() {
     float temperature, humidity;
-    if (sht30_read_data(&temperature, &humidity)) {
+    sht30_read_data(&temperature, &humidity);
         return temperature;
-    } else {
-        printf("Error reading from SHT30\n");
-        return 0.0;
-    }
 }
 
 // Read humidity from SHT30 sensor
 float read_humidity_from_sht30() {
     float temperature, humidity;
-    if (sht30_read_data(&temperature, &humidity)) {
+    sht30_read_data(&temperature, &humidity);
         return humidity;
-    } else {
-        printf("Error reading from SHT30\n");
-        return 0.0;
-    }
 }
 
 float read_temperature_from_bmp280(bmp280 *device) {
     bmp280_read_temperature(device);
-    return device->temperature;
+        return device->temperature;
 }
 
 float read_pressure_from_bmp280(bmp280 *device) {
     bmp280_read_pressure(device);
-    return device->pressure;
+        return device->pressure;
 }
 
 // Read all sensor data
